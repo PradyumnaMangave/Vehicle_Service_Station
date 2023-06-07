5aedd21ca93155f0e7721c2e53bf57bee18b09c4
PradyumnaMangave <mangavepradyumna.gmail.com>
2023-06-07 22:56:52 +0530

    Modification of main class file.

--- src/main/java/com/sunbeam/Main/Main.java
+++ src/main/java/com/sunbeam/Main/Main.java
@@ -4,7 +4,11 @@ import com.sunbeam.Menu.Menu;
 
 public class Main {
 
-	public static void main(){
+	public static void main(String[] args){
+		
+		System.out.println("*******************************");
+		System.out.println("*******Universal Garage*******");
+		System.out.println("*******************************");
 		Menu.mainMenu();
 	}
 }
