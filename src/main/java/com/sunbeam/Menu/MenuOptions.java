457022845f9a53e18f7ce15e3373b787665bdfd8
PradyumnaMangave <mangavepradyumna.gmail.com>
2023-06-07 22:57:48 +0530

    Menu options created to display on main console

--- src/main/java/com/sunbeam/Menu/MenuOptions.java
+++ src/main/java/com/sunbeam/Menu/MenuOptions.java
@@ -1,5 +1,20 @@
 package com.sunbeam.Menu;
 
+import java.util.Scanner;
+
 public class MenuOptions {
 
+	public static int MainMenuOptions() {
+		System.out.println("Main Menu");
+		System.out.println("1. Customer Info");
+		System.out.println("2. Vehicle Info");
+		System.out.println("3. Service Request");
+		System.out.println("4. Part Info");
+		System.out.println("5. Todays Business");
+		System.out.println("6. Given Date Business");
+		System.out.println("0. Exit");
+		return new Scanner(System.in).nextInt();
+		
+		
+	}
 }
