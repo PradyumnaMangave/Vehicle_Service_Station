6ce438bd3d38f2d4d11540987231881e7952c3a8
PradyumnaMangave <mangavepradyumna.gmail.com>
2023-06-09 12:39:57 +0530

    modified insertion menu

--- src/main/java/com/sunbeam/service/Customer_Service.java
+++ src/main/java/com/sunbeam/service/Customer_Service.java
@@ -8,10 +8,21 @@ public class Customer_Service {
 
 	public static void insertToDB() {
 		
+		
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Insert Customer data");
-		System.out.println("Enter Customer Details: id,name,mobile,email,address");
-		Customer_Entity customer = new Customer_Entity(sc.nextInt(), sc.next(), sc.next(), sc.next(), sc.next());
+		System.out.println("Enter ID::");
+		int id = sc.nextInt();
+		System.out.println("Enter Customer Name::");
+		String name = sc.next();
+		System.out.println("Enter Mobile Number::");
+		String mobile = sc.next();
+		System.out.println("Enter Email Address::");
+		String email = sc.next();
+		System.out.println("Enter Address::");
+		String address = sc.next();
+		Customer_Entity customer = new Customer_Entity(id,name,mobile,email,address);
+
 		com.sunbeam.dao.Customer_DAO.insertToDB(customer);
+		System.out.println("Data Inserted Successfully");
 	}
 }
