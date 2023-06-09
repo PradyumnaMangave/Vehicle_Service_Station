b4c2fdcfc34c74bb4c132d9e2b5eef755ffaa13a
PradyumnaMangave <mangavepradyumna.gmail.com>
2023-06-09 10:36:39 +0530

    Entity class created for customer table

---
+++ src/main/java/com/sunbeam/entities/Customer_Entity.java
@@ -0,0 +1,54 @@
+package com.sunbeam.entities;
+
+public class Customer_Entity {
+	
+	private int id;
+	private String name;
+	private String mobile;
+	private String email;
+	private String address;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getMobile() {
+		return mobile;
+	}
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public Customer_Entity(int id, String name, String mobile, String email, String address) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.mobile = mobile;
+		this.email = email;
+		this.address = address;
+	}
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", name=" + name + ", mobile=" + mobile + ", email=" + email + ", address="
+				+ address + "]";
+	}
+	
+}
