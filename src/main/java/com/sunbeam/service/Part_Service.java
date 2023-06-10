6be34af777871d1020f01c9049bec59da980b6a5
PradyumnaMangave <mangavepradyumna.gmail.com>
2023-06-11 01:12:27 +0530

    created service layer to insert data into customer table.

--- src/main/java/com/sunbeam/service/Part_Service.java
+++ src/main/java/com/sunbeam/service/Part_Service.java
@@ -1,11 +1,13 @@
 package com.sunbeam.service;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 import com.sunbeam.dao.Customer_DAO;
 import com.sunbeam.dao.Parts_DAO;
+import com.sunbeam.entities.Customer_Entity;
 import com.sunbeam.entities.Part_Entity;
 
 public class Part_Service {
@@ -25,5 +27,21 @@ public class Part_Service {
 		int id = new Scanner(System.in).nextInt();
 		
 		boolean part = com.sunbeam.dao.Parts_DAO.SpeciShow(id);
+	}
+	
+	public static void insertToDB() {
+		System.out.println("Enter ID::");
+		int id = new Scanner(System.in).nextInt();
+		System.out.println("Enter Part Name::");
+		String name = new Scanner(System.in).next();
+		System.out.println("Enter Description::");
+		String description = new Scanner(System.in).next();
+		System.out.println("Enter Price::");
+		BigDecimal price = new Scanner(System.in).nextBigDecimal();
+
+		Part_Entity parts = new Part_Entity(id,name,description,price);
+
+		com.sunbeam.dao.Parts_DAO.insertToDB(parts);
+		System.out.println("Data Inserted Successfully");
 	}
 }
