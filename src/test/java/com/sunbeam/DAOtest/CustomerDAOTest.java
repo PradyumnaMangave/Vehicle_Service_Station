48a93ebd16c13f247b0722e513f831189cc0a4f0
PradyumnaMangave <mangavepradyumna.gmail.com>
2023-06-13 10:04:25 +0530

    removed unused import

--- src/test/java/com/sunbeam/DAOtest/CustomerDAOTest.java
+++ src/test/java/com/sunbeam/DAOtest/CustomerDAOTest.java
@@ -1,11 +1,6 @@
 package com.sunbeam.DAOtest;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.junit.jupiter.api.Test;
 
 import com.sunbeam.dao.Customer_DAO;
