1ea91fa4b13cc85351fbedcd637752c658a2191c
PradyumnaMangave <mangavepradyumna.gmail.com>
2023-06-13 10:06:18 +0530

    Service layer to insert vehicle data created

--- src/main/java/com/sunbeam/service/Vehicle_service.java
+++ src/main/java/com/sunbeam/service/Vehicle_service.java
@@ -1,21 +1,39 @@
 package com.sunbeam.service;
 
+import java.util.List;
 import java.util.Scanner;
 
+import com.sunbeam.dao.Vehicle_DAO;
+import com.sunbeam.entities.Customer_Entity;
 import com.sunbeam.entities.Vehicle_Entity;
 
 public class Vehicle_service {
 	
 	public static void insertToDb() {
-		System.out.println("Enter ID::");
-		int id = new Scanner(System.in).nextInt();
-		System.out.println("Enter Company Name of Vehicle::");
-		String company = new Scanner(System.in).next(); 
-		System.out.println("Enter Model Name of Vehicle::");
-		String moddel = new Scanner(System.in).next(); 
-		Vehicle_Entity vehicle_Entity = new Vehicle_Entity(id, company, moddel);
-		com.sunbeam.dao.Vehicle_DAO.insertToDB(vehicle_Entity);
-		System.out.println("Data Inserted Successfully");
+		Customer_Entity customer = Customer_Service.SpeciShow1();
+		
+		 if(customer != null) {
+			 System.out.println("Enter vehicle Number::");
+			 String vehicle_number = new Scanner(System.in).next().toUpperCase();
+		
+			 try {
+				 Vehicle_DAO vehicle_DAO= new Vehicle_DAO();
+				 List<Vehicle_Entity> vehicle_list = vehicle_DAO.getData();
+				 for (Vehicle_Entity vehicle_Entity : vehicle_list) {
+					 System.out.println(vehicle_Entity);
+					 }
+				 System.out.println("Choose vehicle");
+				 
+			
+			 } catch (Exception e) {
+				 e.printStackTrace();
+				 }
+			 }
+		 else{
+			 System.out.println("Customer not found....");
+			 System.out.println("Check Again Or Add in Customer Database..");
+			 com.sunbeam.service.Customer_Service.insertToDB();
+				}
 		
 	}
 }
