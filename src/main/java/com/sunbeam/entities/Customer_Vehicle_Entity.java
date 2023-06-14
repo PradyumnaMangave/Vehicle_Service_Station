18cf461d95551f73437a34dc666d6d266b1e258e
PradyumnaMangave <mangavepradyumna.gmail.com>
2023-06-14 15:35:14 +0530

    modified with extra variable.

--- src/main/java/com/sunbeam/entities/Customer_Vehicle_Entity.java
+++ src/main/java/com/sunbeam/entities/Customer_Vehicle_Entity.java
@@ -1,38 +1,48 @@
 package com.sunbeam.entities;
 
 public class Customer_Vehicle_Entity {
-
-	private String vehicle_number;
-	private int customer_id;
-	private int vehicle_id;
-	public String getVehicle_number() {
-		return vehicle_number;
-	}
-	public void setVehicle_number(String vehicle_number) {
-		this.vehicle_number = vehicle_number;
-	}
-	public int getCustomer_id() {
-		return customer_id;
-	}
-	public void setCustomer_id(int customer_id) {
-		this.customer_id = customer_id;
-	}
-	public int getVehicle_id() {
-		return vehicle_id;
-	}
-	public void setVehicle_id(int vehicle_id) {
-		this.vehicle_id = vehicle_id;
-	}
-	public Customer_Vehicle_Entity(String vehicle_number, int customer_id, int vehicle_id) {
-		super();
-		this.vehicle_number = vehicle_number;
-		this.customer_id = customer_id;
-		this.vehicle_id = vehicle_id;
-	}
-	@Override
-	public String toString() {
-		return "Customer_Vehicle_Entity [vehicle_number=" + vehicle_number + ", customer_id=" + customer_id
-				+ ", vehicle_id=" + vehicle_id + "]";
-	}
+	
+	 private int id;
+	    private String vehicleNumber;
+	    private int customerId;
+	    private int vehicleId;
+		public int getId() {
+			return id;
+		}
+		public void setId(int id) {
+			this.id = id;
+		}
+		public String getVehicleNumber() {
+			return vehicleNumber;
+		}
+		public void setVehicleNumber(String vehicleNumber) {
+			this.vehicleNumber = vehicleNumber;
+		}
+		public int getCustomerId() {
+			return customerId;
+		}
+		public void setCustomerId(int customerId) {
+			this.customerId = customerId;
+		}
+		public int getVehicleId() {
+			return vehicleId;
+		}
+		public void setVehicleId(int vehicleId) {
+			this.vehicleId = vehicleId;
+		}
+		public Customer_Vehicle_Entity(int id, String vehicleNumber, int customerId, int vehicleId) {
+			super();
+			this.id = id;
+			this.vehicleNumber = vehicleNumber;
+			this.customerId = customerId;
+			this.vehicleId = vehicleId;
+		}
+		@Override
+		public String toString() {
+			return "Customer_Vehicle_Entity [id=" + id + ", vehicleNumber=" + vehicleNumber + ", customerId="
+					+ customerId + ", vehicleId=" + vehicleId + "]";
+		} 
+		
+	
 	
 }
