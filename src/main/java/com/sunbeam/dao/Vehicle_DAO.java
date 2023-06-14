6c9fc5cdbda90bbe9821409c44b8e4009645eae5
PradyumnaMangave <mangavepradyumna.gmail.com>
2023-06-14 15:37:09 +0530

    DAO layer to show all vehicles of specific customer

--- src/main/java/com/sunbeam/dao/Vehicle_DAO.java
+++ src/main/java/com/sunbeam/dao/Vehicle_DAO.java
@@ -8,6 +8,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.sunbeam.entities.Customer_Entity;
+import com.sunbeam.entities.Customer_Vehicle_Entity;
+import com.sunbeam.entities.NewVehicleEntity;
 import com.sunbeam.entities.Vehicle_Entity;
 import com.sunbeam.util.DatabaseConnectivity;
 
@@ -67,6 +69,20 @@ public class Vehicle_DAO {
 			}
 	}
 
-	
-
+	public void speciCustomer(List<NewVehicleEntity> vehicleList,Customer_Entity cust) {
+		try {
+			Connection con = DatabaseConnectivity.create();
+			String SQL = "SELECT v.company, v.model, cv.vehicle_number FROM vehicle v INNER JOIN customer_vehicle cv ON v.id = cv.vehicle_id WHERE cv.customer_id = ?";
+			PreparedStatement pst = con.prepareStatement(SQL);
+			pst.setInt(1, cust.getId());
+			ResultSet rs = pst.executeQuery();
+			while(rs.next()) {
+				NewVehicleEntity vehicle = new NewVehicleEntity(rs.getString(1), rs.getString(2), rs.getString(3));
+				vehicleList.add(vehicle);
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
