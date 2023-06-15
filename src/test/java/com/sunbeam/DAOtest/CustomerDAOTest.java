c8618a9dc4612c6c14df09c9c54212b742e86cc8
PradyumnaMangave <mangavepradyumna.gmail.com>
2023-06-15 15:13:51 +0530

    DAO test cases created for delete functionality

--- src/test/java/com/sunbeam/DAOtest/CustomerDAOTest.java
+++ src/test/java/com/sunbeam/DAOtest/CustomerDAOTest.java
@@ -1,5 +1,7 @@
 package com.sunbeam.DAOtest;
 
+import static org.junit.Assert.assertTrue;
+
 import java.sql.SQLException;
 import org.junit.jupiter.api.Test;
 
@@ -14,6 +16,14 @@ class CustomerDAOTest {
 			Customer_Entity cust = new Customer_Entity(9, "test", "9988777788", "test", "test");
 			System.out.println(Customer_DAO.insertToDB(cust));
 
+	}
+	
+	@Test
+	void testDeleteFromDB() throws SQLException {
+	    Customer_Entity cust = new Customer_Entity(17, "test", "9988777788", "test", "test");
+	    Customer_DAO.insertToDB(cust);
+	    boolean deletionSuccessful = Customer_DAO.Delete(cust.getId());
+	    assertTrue(deletionSuccessful);
 	}
 
 }
