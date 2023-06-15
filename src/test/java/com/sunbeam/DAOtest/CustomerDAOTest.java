1a2e766ceb77667110148c514d00ee4cb7be5ce5
PradyumnaMangave <mangavepradyumna.gmail.com>
2023-06-15 15:34:54 +0530

    DAO Test cases created to check update functionality

--- src/test/java/com/sunbeam/DAOtest/CustomerDAOTest.java
+++ src/test/java/com/sunbeam/DAOtest/CustomerDAOTest.java
@@ -1,5 +1,6 @@
 package com.sunbeam.DAOtest;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.sql.SQLException;
@@ -25,5 +26,34 @@ class CustomerDAOTest {
 	    boolean deletionSuccessful = Customer_DAO.Delete(cust.getId());
 	    assertTrue(deletionSuccessful);
 	}
+	
+	@Test
+	void testUpdateExistingCustomer() throws SQLException {
+
+	    
+	    int id = 18;
+	    String mobile = "9988777788";
+	    String address = "test";
+	    Customer_DAO.insertToDB(new Customer_Entity(id, "test", mobile, address, "test"));
+
+	    String newMobile = "9876543210";
+	    String newAddress = "updatedAddress";
+	    boolean updateSuccessful = Customer_DAO.update(id, newMobile, newAddress);
+	    
+	    assertTrue(updateSuccessful);
+	}
+
+	@Test
+	void testUpdateNonExistingCustomer() throws SQLException {
+
+	    
+	    int id = 99;
+	    String mobile = "9876543210";
+	    String address = "updatedAddress";
+	    boolean updateSuccessful = Customer_DAO.update(id, mobile, address);
+
+	    assertFalse(updateSuccessful);
+	}
+
 
 }
