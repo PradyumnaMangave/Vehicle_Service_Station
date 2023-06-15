9c476a05b96e62460d0a04e0cc6e3fdcadcdb6bb
PradyumnaMangave <mangavepradyumna.gmail.com>
2023-06-15 10:05:47 +0530

    DAO layer to show all vehicles created

--- src/main/java/com/sunbeam/dao/Vehicle_DAO.java
+++ src/main/java/com/sunbeam/dao/Vehicle_DAO.java
@@ -72,12 +72,28 @@ public class Vehicle_DAO {
 	public void speciCustomer(List<NewVehicleEntity> vehicleList,Customer_Entity cust) {
 		try {
 			Connection con = DatabaseConnectivity.create();
-			String SQL = "SELECT v.company, v.model, cv.vehicle_number FROM vehicle v INNER JOIN customer_vehicle cv ON v.id = cv.vehicle_id WHERE cv.customer_id = ?";
+			String SQL = "SELECT c.name ,v.company, v.model, cv.vehicle_number FROM vehicle v INNER JOIN customer_vehicle cv ON v.id = cv.vehicle_id INNER JOIN customer c ON c.id=cv.customer_id WHERE cv.customer_id = ?";
 			PreparedStatement pst = con.prepareStatement(SQL);
 			pst.setInt(1, cust.getId());
 			ResultSet rs = pst.executeQuery();
 			while(rs.next()) {
-				NewVehicleEntity vehicle = new NewVehicleEntity(rs.getString(1), rs.getString(2), rs.getString(3));
+				NewVehicleEntity vehicle = new NewVehicleEntity(rs.getString(1),rs.getString(4), rs.getString(2), rs.getString(3));
+				vehicleList.add(vehicle);
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void ShowAll(List<NewVehicleEntity> vehicleList,Customer_Entity cust) {
+		try {
+			Connection con = DatabaseConnectivity.create();
+			String SQL = "SELECT c.name,v.company, v.model, cv.vehicle_number FROM vehicle v INNER JOIN customer_vehicle cv ON v.id = cv.vehicle_id INNER JOIN customer c ON c.id=cv.customer_id";
+			PreparedStatement pst = con.prepareStatement(SQL);
+			ResultSet rs = pst.executeQuery();
+			while(rs.next()) {
+				NewVehicleEntity vehicle = new NewVehicleEntity(rs.getString(1),rs.getString(4), rs.getString(2), rs.getString(3));
 				vehicleList.add(vehicle);
 			}
 			
