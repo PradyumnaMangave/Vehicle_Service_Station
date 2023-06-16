8c35c3b1b34c8879679a6f15bbad20ba41bf1686
PradyumnaMangave <mangavepradyumna.gmail.com>
2023-06-16 11:57:09 +0530

    Written DAO test case to check specific show vehicle functionality

--- src/test/java/com/sunbeam/DAOtest/VehicleDAOtest.java
+++ src/test/java/com/sunbeam/DAOtest/VehicleDAOtest.java
@@ -1,9 +1,15 @@
 package com.sunbeam.DAOtest;
 
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 import com.sunbeam.dao.Vehicle_DAO;
-import com.sunbeam.entities.Customer_Vehicle_Entity;
+import com.sunbeam.entities.Customer_Entity;
+import com.sunbeam.entities.NewVehicleEntity;
 import com.sunbeam.entities.Vehicle_Entity;
 
 class VehicleDAOtest {
@@ -26,8 +32,16 @@ class VehicleDAOtest {
 		System.out.println("Done");
 		}
 
+	@Test
+	void testSpeciShow() {
+		List<NewVehicleEntity> list = new ArrayList<NewVehicleEntity>();
+		Vehicle_DAO vehi = new Vehicle_DAO();
+		Customer_Entity cust = new Customer_Entity(0, null, null, null, null);
+		cust.setId(9);
+		vehi.speciCustomer(list, cust);
+		assertTrue(true);
+		System.out.println("done");
 	}
-
-	
+}
 
 
