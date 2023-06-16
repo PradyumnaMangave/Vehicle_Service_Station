52596b9c0b39988c0ca7df111601ef002050bb9b
PradyumnaMangave <mangavepradyumna.gmail.com>
2023-06-16 14:24:36 +0530

    Written DAO test cases to check delete vehicle and specific customer functionality of vehicle

--- src/test/java/com/sunbeam/DAOtest/VehicleDAOtest.java
+++ src/test/java/com/sunbeam/DAOtest/VehicleDAOtest.java
@@ -64,6 +64,27 @@ class VehicleDAOtest {
 	     boolean result1 = vehi.searchNumber(existingNumber);
 	     System.out.println("Vehicle number found: " + result1);
 	}
+	
+	@Test
+	public void testSpeciCustomerByVehiNum() {
+		 String existingVehicleNumber = "MH23FM9306";
+	        List<NewVehicleEntity> vehicleList = new ArrayList<>();
+	        Customer_Entity cust = new Customer_Entity(0, existingVehicleNumber, existingVehicleNumber, existingVehicleNumber, existingVehicleNumber);
+	        Vehicle_DAO vehi = new Vehicle_DAO();
+	        vehi.speciCustomerByVehiNum(vehicleList, cust, existingVehicleNumber);
+	        System.out.println("Vehicle List:");
+	        for (NewVehicleEntity vehicle : vehicleList) {
+	            System.out.println(vehicle);
+	        }
+	}
+	
+	@Test
+	public void testDelete() {
+		String existingVehicleNumber = "MH12JH4321";
+		Vehicle_DAO vehi = new Vehicle_DAO();
+        boolean result1 = vehi.Delete(existingVehicleNumber);
+        System.out.println("Vehicle deleted: " + result1);
+	}
 }
 
 
