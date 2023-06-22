54ed5b92ad60626ece7e2c9f7a505e07c374595f
PradyumnaMangave <mangavepradyumna.gmail.com>
2023-06-22 21:09:12 +0530

    written unimplemented methods

--- src/main/java/com/sunbeam/entities/oil.java
+++ src/main/java/com/sunbeam/entities/oil.java
@@ -1,6 +1,6 @@
 	package com.sunbeam.entities;
 
-public abstract class oil extends Service{
+public class oil extends Service{
 
 	private double oil_cost;
 
@@ -14,12 +14,25 @@ public abstract class oil extends Service{
 
 	public oil(double oil_cost) {
 		super("oil");
-		this.oil_cost = oil_cost;
+		this.oil_cost = 0;
 	}
+	
 
 	@Override
 	public String toString() {
 		return "oil [oil_cost=" + oil_cost + "]";
+	}
+
+	@Override
+	public void acceptService() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void calculateTotalCost() {
+		// TODO Auto-generated method stub
+		
 	}
 	
 	
