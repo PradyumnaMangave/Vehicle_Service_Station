75b06459f18391fb8595156e2d6c473daa3e13a4
PradyumnaMangave <mangavepradyumna.gmail.com>
2023-06-22 20:46:40 +0530

    Created service layer to insert maintainance data done for service req. into database

---
+++ src/main/java/com/sunbeam/service/Service_service.java
@@ -0,0 +1,52 @@
+package com.sunbeam.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import com.sunbeam.dao.serviceDAO;
+import com.sunbeam.entities.Service;
+import com.sunbeam.entities.ServiceRequestEntity;
+import com.sunbeam.entities.maintainance;
+
+public class Service_service {
+
+	public static void getAllService(ServiceRequestEntity serviceRequest) {
+		
+		List<Service> serviceList = com.sunbeam.dao.serviceDAO.getAllServices(serviceRequest);
+		//System.out.println(serviceList);
+		System.out.println(serviceRequest.getServiceList());
+	}
+
+	public static void doMaintainance(ServiceRequestEntity serviceRequest) {
+		List<Service> serviceList = serviceRequest.getServiceList();
+		maintainance service = null;
+		boolean serviceFound = false;
+		if(serviceList.isEmpty()) {
+			service = new maintainance();
+			serviceList.add(service);
+			service.acceptService();
+			serviceDAO.createNewMaintainance(serviceRequest, service);
+		}
+		else {
+			for(Service S : serviceList) {
+				if(S instanceof maintainance) {
+						service = (maintainance) S;
+				serviceFound = true;
+			}
+		}
+			if(!serviceFound) {
+				service = new maintainance();
+				serviceList.add(service);
+				service.acceptService();
+				serviceDAO.createNewMaintainance(serviceRequest, service);
+			}
+		
+	}
+		
+	} 
+	
+	
+
+    
+	}
