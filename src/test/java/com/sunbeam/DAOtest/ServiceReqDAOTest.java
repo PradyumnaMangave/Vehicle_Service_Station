b4b9975b111c5c49fccfa1a85038a370b6f3cdfd
PradyumnaMangave <mangavepradyumna.gmail.com>
2023-06-23 14:40:05 +0530

    written DAO test cases for service request dao

--- src/test/java/com/sunbeam/DAOtest/ServiceReqDAOTest.java
+++ src/test/java/com/sunbeam/DAOtest/ServiceReqDAOTest.java
@@ -5,14 +5,18 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import com.sunbeam.dao.ServiceReqDAO;
 import com.sunbeam.entities.Customer_Entity;
+import com.sunbeam.entities.ServiceRequestEntity;
 
 class ServiceReqDAOTest<NewVehicleEntity> {
 
-	 @Test
+	 private ServiceRequestEntity result;
+
+	@Test
 	    public void testGetVehicleByID() {
 	        List<NewVehicleEntity> vehicleList = new ArrayList<>();
 	        Customer_Entity cust = new Customer_Entity(0, null, null, null, null);
@@ -22,5 +26,31 @@ class ServiceReqDAOTest<NewVehicleEntity> {
 	        System.out.println("Tested");
 	        
 	    }
-
+	 
+	 @Test
+	 public void testAddIntoServices() {
+		 ServiceReqDAO serviceRequestDao = new ServiceReqDAO();
+	      String vehicleNumber = "mh09da9306";
+	       result = serviceRequestDao.addIntoServices(vehicleNumber);
+	 }
+	 
+	 @Test
+	 public void testGetAllServiceRequests() {
+		 ServiceReqDAO serviceRequestDao = new ServiceReqDAO();
+	        
+	       
+	        List<ServiceRequestEntity> serviceRequests = serviceRequestDao.getAllServiceRequests();
+	        
+	        // Perform assertions to verify the results
+	        Assertions.assertNotNull(serviceRequests);
+	        Assertions.assertFalse(serviceRequests.isEmpty());
+	        System.out.println("Tetsted");
+	 }
+	 
+	 @Test
+	 public void testGetSpecificDayServiceRequests() {
+		 ServiceReqDAO serviceRequestDao = new ServiceReqDAO();
+	        List<ServiceRequestEntity> serviceRequests = serviceRequestDao.getSpecificDayServiceRequests(null);
+	        System.out.println("Tetsted");
+	 }
 }
